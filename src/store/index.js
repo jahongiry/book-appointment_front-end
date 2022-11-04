17e85b05eafa3b63bfdd0b403d54cc9bc31d46c4
Zelalem1222 <zelalemfissha394@gmail.com>
2022-11-04 11:12:12 +0300

    Modify the store reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore , combineReducers } from "@reduxjs/toolkit";
 import carsReducer from "./mainpage_reducer";
+import { userReducer } from "./user_reducer";
 
 const rootReducer = combineReducers({
   cars: carsReducer,
+  user: userReducer,
+
 })
 
 const store = configureStore({
