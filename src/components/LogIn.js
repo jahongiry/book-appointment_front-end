721e84f5a4dfd13e4333aad25a3a3bf7f26cdd33
Zelalem1222 <zelalemfissha394@gmail.com>
2022-11-04 11:11:22 +0300

    MOdify login page

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,40 +1,56 @@
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { sendSigninDetails } from '../store/user_reducer';
 import "./LogIn.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function LogIn() {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.user);
+  console.log(state);
+  const { loggedIn } = state;
+  const [signIn , setSignIn] = useState(loggedIn)
+  const [username, setUsername] = useState('');
+  const loginUser = (e) => {
+    e.preventDefault();
+    const user = {
+      username,
+    };
+    dispatch(sendSigninDetails(user));
+    setUsername('')
+  }
+
+  useEffect(() => {
+    setSignIn(loggedIn)
+    if (loggedIn === 'in') {
+      navigate('/mainpage') , { replace: true }
+    }
+  }, [state]);
+
+
+
   return (
     <div className="login-container">
       <h1 className="login-heading">TRY NEW LUXURY CAR MODELS WITH US!</h1>
-      <form className="email-login login-info">
+      <form className="email-login login-info" onSubmit={loginUser}>
+        
         <label>
           <input
             className="input-email"
             type="text"
             name="username"
-            placeholder="Enter your username..."
+            placeholder="Enter User name..."
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
           />
         </label>
-        <label>
-          <input
-            className="input-email"
-            type="text"
-            name="username"
-            placeholder="Enter first name..."
-          />
-        </label>
-        <label>
-          <input
-            className="input-email"
-            type="text"
-            name="username"
-            placeholder="Enter last name..."
-          />
-        </label>
-        <Link to="/mainpage">
-        <button className="btn btn-success">Login</button>
-        </Link>
+        
+        <button type="submit" className="btn btn-success" >Login</button>
+        
       </form>
     </div>
   );
