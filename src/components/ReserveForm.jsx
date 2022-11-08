6be3314f2b8d3f6c37176ca784809d10c6cae861
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-11-08 14:56:24 +0500

    Add styling to forms

--- src/components/ReserveForm.jsx
+++ src/components/ReserveForm.jsx
@@ -1,82 +1,110 @@
-import React, { useState , useEffect } from "react"
-import { useSelector , useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { createNewReservation } from "../store/reservationReducer"
-import { fetchAllCars } from '../store/mainpage_reducer'
-
-
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createNewReservation } from "../store/reservationReducer";
+import { fetchAllCars } from "../store/mainpage_reducer";
+import "./ReserveForm.css";
 
 const ReserveForm = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const cars = useSelector(state => state.cars.cars)
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cars = useSelector((state) => state.cars.cars);
 
   const { userId } = JSON.parse(window.localStorage.getItem("user"));
   const userid = userId || 1;
-  const [date, setDate] = useState('')
-  const [city, setCity] = useState('')
-  const [carId, setCarId] = useState()
-  console.log('carId', carId)
+  const [date, setDate] = useState("");
+  const [city, setCity] = useState("");
+  const [carId, setCarId] = useState();
+  console.log("carId", carId);
   const newReservation = (e) => {
-    e.preventDefault()
-    dispatch(createNewReservation({userid , carId ,   date , city ,}))
-    setCarId()
-    setCity('')
-    setDate('')
-    navigate('/reservations')
-  }
+    e.preventDefault();
+    dispatch(createNewReservation({ userid, carId, date, city }));
+    setCarId();
+    setCity("");
+    setDate("");
+    navigate("/reservations");
+  };
 
+  useEffect(() => {
+    dispatch(fetchAllCars());
+  }, [dispatch]);
 
-
-useEffect(() => {
-  dispatch(fetchAllCars())
-}, [dispatch])
-
-    return (
-  
-      
-      <div className='main'>
-        <h1>Reservation Form</h1>
-        <div className="card" style={{backgroundColor: 'lightgreen'}}>
-          <form onSubmit={newReservation}>
-        <div className='form-group'>
-       
-          <label className="form-lable"><strong>Choose A Car</strong></label>
-          <select className="form-control form-control-lg" onChange={(e) =>  
-          setCarId(e.target.options[e.target.selectedIndex].value || e.target.options[0].value )
-        }
-          
-          placeholder='Choose Car'>
-           {cars.map((car) => {
-                return <option key={car.id} value={car.id} placeholder="Choose A car">{car.name}</option>
-           })}
-
-          </select>
-           
+  return (
+    <div className="main">
+      <h1>Reservation Form</h1>
+      <div
+        className="card reserve-car-d"
+        style={{ backgroundColor: "lightgreen" }}
+      >
+        <form onSubmit={newReservation}>
+          <div className="form-group">
+            <label className="form-lable">
+              <strong>Choose A Car</strong>
+            </label>
+            <select
+              className="form-control form-control-lg"
+              onChange={(e) =>
+                setCarId(
+                  e.target.options[e.target.selectedIndex].value ||
+                    e.target.options[0].value
+                )
+              }
+              placeholder="Choose Car"
+            >
+              {cars.map((car) => {
+                return (
+                  <option
+                    key={car.id}
+                    value={car.id}
+                    placeholder="Choose A car"
+                  >
+                    {car.name}
+                  </option>
+                );
+              })}
+            </select>
           </div>
-          <div className='form-group'>
-       
-          <label><strong>Choose A Date</strong></label>
-          <input className="form-control form-control-lg" type='date' value={date}
-          onChange={(e)=> {setDate(e.target.value)} }/>
-           
+          <div className="form-group">
+            <label>
+              <strong>Choose A Date</strong>
+            </label>
+            <input
+              className="form-control form-control-l input-r"
+              type="date"
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
           </div>
-          <input type='hidden' value={carId} onChange={()=> {setCarId(cars)} }/>
-          <div className='form-group '>
-       
-          <label><strong>Please Enter City</strong></label>
-          <input className="form-control form-control-lg" type='text' value={city}
-          onChange={(e)=>{setCity(e.target.value)}}
+          <input
+            className="input-r"
+            type="hidden"
+            value={carId}
+            onChange={() => {
+              setCarId(cars);
+            }}
           />
-           </div>
-           <button className="btn btn-dark" type='submit' >Make Reservation </button>
-      </form>
-      </div> 
+          <div className="form-group ">
+            <label>
+              <strong>Please Enter City</strong>
+            </label>
+            <input
+              className="form-control form-control-lg input-r"
+              type="text"
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+            />
+          </div>
+          <button className="btn btn-dark button-r" type="submit">
+            Make Reservation{" "}
+          </button>
+        </form>
       </div>
-     
-      
-    )
-  }
+    </div>
+  );
+};
 
-  export default ReserveForm
+export default ReserveForm;
--- src/components/reserveCar.jsx
+++ src/components/reserveCar.jsx
@@ -1,67 +1,86 @@
-import React,{ useState } from 'react'
-import { useSelector , useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { createNewReservation } from "../store/reservationReducer";
+import "./ReserveCar.css";
 
 function ReserveCar() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const car = useSelector(state => state.cars.car)
-    console.log('car', car)
-    const { userId } = JSON.parse(window.localStorage.getItem("user"));
-    const userid = userId;
-    const [date, setDate] = useState('')
-    const [city, setCity] = useState('')
-    const [carId, setCarId] = useState(car.id)
-    console.log('carId', carId)
-    const newReservation = (e) => {
-      e.preventDefault()
-      dispatch(createNewReservation({userid , carId ,   date , city ,}))
-      setCarId()
-      setCity('')
-      setDate('')
-      navigate('/reservations')
-    }
-  
-  
-  
-//   useEffect(() => {
-//     dispatch(fetchAllCars())
-//   }, [dispatch])
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const car = useSelector((state) => state.cars.car);
+  console.log("car", car);
+  const { userId } = JSON.parse(window.localStorage.getItem("user"));
+  const userid = userId;
+  const [date, setDate] = useState("");
+  const [city, setCity] = useState("");
+  const [carId, setCarId] = useState(car.id);
+  console.log("carId", carId);
+  const newReservation = (e) => {
+    e.preventDefault();
+    dispatch(createNewReservation({ userid, carId, date, city }));
+    setCarId();
+    setCity("");
+    setDate("");
+    navigate("/reservations");
+  };
+
   return (
     <div>
-      <div className='main'>
+      <div className="main">
         <h1>Reservation Form</h1>
-        <div className="card" style={{backgroundColor: 'lightgreen'}}>
+        <div
+          className="card reserve-car-d"
+          style={{ backgroundColor: "lightgreen" }}
+        >
           <form onSubmit={newReservation}>
-        <div className='form-group'>
-       
-          <label className="form-lable"><strong>Choosen Car</strong></label>
-          <input type="text" value={car.name} />
-           
-          </div>
-          <div className='form-group'>
-       
-          <label><strong>Choose A Date</strong></label>
-          <input className="form-control form-control-lg" type='date' value={date}
-          onChange={(e)=> {setDate(e.target.value)} }/>
-           
-          </div>
-          <input type='hidden' value={carId} onChange={()=> {setCarId(cars)} }/>
-          <div className='form-group '>
-       
-          <label><strong>Please Enter City</strong></label>
-          <input className="form-control form-control-lg" type='text' value={city}
-          onChange={(e)=>{setCity(e.target.value)}}
-          />
-           </div>
-           <button className="btn btn-dark" type='submit' >Make Reservation </button>
-      </form>
-      </div> 
+            <div className="form-group">
+              <label className="form-lable">
+                <strong>Choosen Car</strong>
+              </label>
+              <input className="input-r" type="text" value={car.name} />
+            </div>
+            <div className="form-group">
+              <label>
+                <strong>Choose A Date</strong>
+              </label>
+              <input
+                className="form-control form-control-lg input-r"
+                type="date"
+                value={date}
+                onChange={(e) => {
+                  setDate(e.target.value);
+                }}
+              />
+            </div>
+            <input
+              className="input-r"
+              type="hidden"
+              value={carId}
+              onChange={() => {
+                setCarId(cars);
+              }}
+            />
+            <div className="form-group ">
+              <label>
+                <strong>Please Enter City</strong>
+              </label>
+              <input
+                className="form-control form-control-lg input-r"
+                type="text"
+                value={city}
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+              />
+            </div>
+            <button className="btn btn-dark button-r" type="submit">
+              Make Reservation{" "}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ReserveCar
+export default ReserveCar;
